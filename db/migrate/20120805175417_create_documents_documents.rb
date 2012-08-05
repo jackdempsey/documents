649228e43b8ecbebb912e755c69a8ac21bbf2077
Jack Dempsey <jack.dempsey@gmail.com>
2012-08-05 12:55:20 -0700

    docs should have a title and description

--- db/migrate/20120805175417_create_documents_documents.rb
+++ db/migrate/20120805175417_create_documents_documents.rb
@@ -1,7 +1,8 @@
 class CreateDocumentsDocuments < ActiveRecord::Migration
   def change
     create_table :documents_documents do |t|
-      t.string :file
+      t.string :file, :title
+      t.text :description
       t.references :owner, polymorphic: true
 
       t.timestamps
