669240b57c42f5b46e88b626a27f0acca544709f
Jack Dempsey <jack.dempsey@gmail.com>
2012-08-05 10:49:38 -0700

    configure engine to use rspec when generating

--- lib/documents/engine.rb
+++ lib/documents/engine.rb
@@ -1,5 +1,9 @@
 module Documents
   class Engine < ::Rails::Engine
     isolate_namespace Documents
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
